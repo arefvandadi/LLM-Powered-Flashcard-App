927d77845d8a6738c8ed43084fe812414906310c
arefvandadi <aref.vandadi@gmail.com>
2024-09-10 14:36:57 -0400

    Add a section for extracting audio using ffmpeg. Also update yt-dlp option dict to only ouput mp4 videos

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import yt_dlp
-import subprocess
+import ffmpeg
 
 
 ##################### Download Youtube Videos to Local Drive ###############################
@@ -10,10 +10,18 @@ SHORTER_YOUTUBE_LINK = "https://youtu.be/65ya2V7Gi74"
 
 # Specifies the output folder for the yt-dlp video downloads
 yt_options = {
-    'outtmpl':"./data/videos/%(title)s.%(ext)s"
+    'outtmpl':"./data/videos/%(title)s.%(ext)s",
+    'merge_output_format': 'mp4'
 }
 
 with yt_dlp.YoutubeDL(yt_options) as ydl:
     #download function below takes a list of urls
     ydl.download([SHORTER_YOUTUBE_LINK])
+
+
+##################### Extracting Audio from Downloaded Videos ###############################
+video_file = ffmpeg.input("./data/videos/Theater of Politics.mp4")
+video_file.output("./data/audio/audio.mp3", acodec="mp3").run()
+
+
 
