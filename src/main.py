bd611a4d52a560cee7b9175d3414c60511b9b93d
arefvandadi <aref.vandadi@gmail.com>
2024-09-10 14:49:01 -0400

    Change audio extraction to output wav instead of mp3

--- src/main.py
+++ src/main.py
@@ -4,24 +4,25 @@ import ffmpeg
 
 ##################### Download Youtube Videos to Local Drive ###############################
 
-YOUTUBE_LINK = "https://youtu.be/rqTl-livoRo"
-SHORTER_YOUTUBE_LINK = "https://youtu.be/65ya2V7Gi74"
+# YOUTUBE_LINK = "https://youtu.be/rqTl-livoRo"
+# SHORTER_YOUTUBE_LINK = "https://youtu.be/65ya2V7Gi74"
 
 
-# Specifies the output folder for the yt-dlp video downloads
-yt_options = {
-    'outtmpl':"./data/videos/%(title)s.%(ext)s",
-    'merge_output_format': 'mp4'
-}
+# # Specifies the output folder for the yt-dlp video downloads
+# yt_options = {
+#     'outtmpl':"./data/videos/%(title)s.%(ext)s",
+#     'merge_output_format': 'mp4'
+# }
 
-with yt_dlp.YoutubeDL(yt_options) as ydl:
-    #download function below takes a list of urls
-    ydl.download([SHORTER_YOUTUBE_LINK])
+# with yt_dlp.YoutubeDL(yt_options) as ydl:
+#     #download function below takes a list of urls
+#     ydl.download([SHORTER_YOUTUBE_LINK])
 
 
 ##################### Extracting Audio from Downloaded Videos ###############################
 video_file = ffmpeg.input("./data/videos/Theater of Politics.mp4")
-video_file.output("./data/audio/audio.mp3", acodec="mp3").run()
+# video_file.output("./data/audio/audio.mp3", acodec="mp3").run()
+video_file.output("./data/audio/audio.wav", acodec="pcm_s16le").run()
 
 
 
