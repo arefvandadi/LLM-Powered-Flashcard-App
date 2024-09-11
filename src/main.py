ffc29c623473b7b8df0ff738d92f387325779641
arefvandadi <aref.vandadi@gmail.com>
2024-09-11 15:41:49 -0400

    Add doc strings to class and methods in media_processor module. Also change method names for clarity.

--- src/main.py
+++ src/main.py
@@ -4,8 +4,11 @@ from media_processor import MediaProcessor
 SHORTER_YOUTUBE_LINK = "https://youtu.be/yY_kCcQ1r64"
 
 youtube_handler = MediaProcessor(youtube_url=SHORTER_YOUTUBE_LINK)
-words_list = youtube_handler.extract_words_from_youtube()
+words_list = youtube_handler.extract_words_from_youtube_pipeline()
 print(words_list)
+print(type(words_list))
+print(words_list[0])
+print(words_list[3])
 
 
 
