f2c714e1049f3234dc11fa3b58210ab09c836bfe
arefvandadi <aref.vandadi@gmail.com>
2024-09-11 21:59:58 -0400

    Add a new method in media_processor module named definition_finder_gpt to return the definitions of the words found in videos

--- src/main.py
+++ src/main.py
@@ -5,11 +5,12 @@ from words_saver import words_saver
 SHORTER_YOUTUBE_LINK = "https://youtu.be/yY_kCcQ1r64"
 
 youtube_handler = MediaProcessor(youtube_url=SHORTER_YOUTUBE_LINK)
-words_list = youtube_handler.extract_words_from_youtube_pipeline()
+words_list, definition_list = youtube_handler.extract_words_from_youtube_pipeline()
 print(words_list)
+print(definition_list)
 # print(type(words_list))
 
-words_saver(words_list)
+# words_saver(words_list)
 
 
 
