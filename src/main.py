01f7b4a4dfaa8ae29bf67529e4f9591fbb6712f6
arefvandadi <aref.vandadi@gmail.com>
2024-09-18 09:51:36 -0400

    Add main guard in main.py to define the script entry point

--- src/main.py
+++ src/main.py
@@ -6,32 +6,36 @@ from media_processor import MediaProcessor
 from word_repository import WordRepoManager
 from app_config import BACKGROUND_COLOR
 
-###################### Main Window #######################
-window = Tk()
-window.title("FlashMind")
-window.config(padx=50, pady=50)
-window.config(bg=BACKGROUND_COLOR)
+def main():
 
-# Word Repository Manager
-word_repo_manager = WordRepoManager(window)
+    ###################### Main Window #######################
+    window = Tk()
+    window.title("FlashMind")
+    window.config(padx=50, pady=50)
+    window.config(bg=BACKGROUND_COLOR)
 
-# Main Canvas for showing words and meanings in the App
-canvas_manager = CanvasManager(word_repo_manager)
-canvas_manager.create_canvas()
+    # Word Repository Manager
+    word_repo_manager = WordRepoManager(window)
 
-# Main Buttons and their functionality
-button_manager = ButtonManager(window, canvas_manager, word_repo_manager)
-button_manager.create_buttons()
+    # Main Canvas for showing words and meanings in the App
+    canvas_manager = CanvasManager(word_repo_manager)
+    canvas_manager.create_canvas()
 
-# Create Manu Bar and Handle Importing Words by Using media_processor and words_saver modules
-menu_bar_manager = MenuBarManager(window, MediaProcessor, canvas_manager, button_manager)
-menu_bar_manager.create_menu()
+    # Main Buttons and their functionality
+    button_manager = ButtonManager(window, canvas_manager, word_repo_manager)
+    button_manager.create_buttons()
 
-
-# Changes the closing protocol of the app window. 
-# It binds the actions of closing the window with the update_word_repo_csv method in word_repository module. 
-window.protocol("WM_DELETE_WINDOW", word_repo_manager.update_word_repo_csv)
-
-window.mainloop()
+    # Create Manu Bar and Handle Importing Words by Using media_processor and words_saver modules
+    menu_bar_manager = MenuBarManager(window, MediaProcessor, canvas_manager, button_manager)
+    menu_bar_manager.create_menu()
 
 
+    # Changes the closing protocol of the app window. 
+    # It binds the actions of closing the window with the update_word_repo_csv method in word_repository module. 
+    window.protocol("WM_DELETE_WINDOW", word_repo_manager.update_word_repo_csv)
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
